824387c66d8e30974b8cccc939e8a79e6e327489
Tad Nicol <tadnicol@me.com>
2025-03-01 13:18:12 -0800

    Created simple visage based gui

--- src/Plugin.cpp
+++ src/Plugin.cpp
@@ -18,3 +18,125 @@ clap_plugin_descriptor Plugin::descriptor = {.clap_version = CLAP_VERSION,
 Plugin::Plugin(const clap_host *host) : ClapPlugin(&Plugin::descriptor, host)
 {
 }
+
+Plugin::~Plugin() = default;
+
+bool Plugin::guiIsApiSupported(char const *api, bool is_floating) noexcept
+{
+    if (is_floating)
+        return false;
+
+#ifdef _WIN32
+    if (strcmp(api, CLAP_WINDOW_API_WIN32) == 0)
+        return true;
+#elif __APPLE__
+    if (strcmp(api, CLAP_WINDOW_API_COCOA) == 0)
+        return true;
+#elif __linux__
+    if (strcmp(api, CLAP_WINDOW_API_X11) == 0)
+        return true;
+#endif
+
+    return false;
+}
+
+bool Plugin::guiCreate(char const */*api*/, bool is_floating) noexcept
+{
+    if (is_floating)
+        return false;
+
+    if (mApp != nullptr)
+        return true;
+
+    mApp = std::make_unique<visage::ApplicationWindow>();
+    mApp->setWindowDimensions(50, 50);
+
+    mApp->onDraw() = [this](visage::Canvas& canvas) {
+        canvas.setColor(0xff000066);
+        canvas.fill(0, 0, mApp->width(), mApp->height());
+
+        float circle_radius = mApp->height() * 0.1f;
+        float x = mApp->width() * 0.5f - circle_radius;
+        float y = mApp->height() * 0.5f - circle_radius;
+        canvas.setColor(0xff00ffff);
+        canvas.circle(x, y, 2.0f * circle_radius);
+    };
+
+    mApp->onWindowContentsResized() = [this] {
+        _host.guiRequestResize(static_cast<uint32_t>(mApp->logicalWidth()),
+                               static_cast<uint32_t>(mApp->logicalHeight()));
+    };
+
+    return true;
+}
+
+void Plugin::guiDestroy() noexcept
+{
+#if __linux__
+    if (mApp && mApp->window() && _host.canUsePosixFdSupport())
+        _host.posixFdSupportUnregister(mApp->window()->posixFd());
+#endif
+
+    mApp->close();
+}
+
+bool Plugin::guiSetParent(clap_window const *window) noexcept
+{
+    if (mApp == nullptr)
+        return false;
+
+    mApp->show(window->ptr);
+
+#if __linux__
+    if (_host.canUsePosixFdSupport() && mApp->window()) {
+        int fd_flags = CLAP_POSIX_FD_READ | CLAP_POSIX_FD_WRITE | CLAP_POSIX_FD_ERROR;
+        return _host.posixFdSupportRegister(mApp->window()->posixFd(), fd_flags);
+    }
+#endif
+    return true;
+}
+
+bool Plugin::guiGetResizeHints(clap_gui_resize_hints_t *hints) noexcept
+{
+    if (mApp == nullptr)
+        return false;
+
+    bool fixed_aspect_ratio = mApp->isFixedAspectRatio();
+    hints->can_resize_horizontally = true;
+    hints->can_resize_vertically = true;
+    hints->preserve_aspect_ratio = fixed_aspect_ratio;
+
+    if (fixed_aspect_ratio) {
+        hints->aspect_ratio_width  = static_cast<uint32_t>(mApp->height() * mApp->aspectRatio());
+        hints->aspect_ratio_height = static_cast<uint32_t>(mApp->width());
+    }
+    return true;
+}
+
+bool Plugin::guiAdjustSize(uint32_t *width, uint32_t *height) noexcept
+{
+    if (mApp == nullptr)
+        return false;
+
+    mApp->adjustWindowDimensions(width, height, true, true);
+    return true;
+}
+
+bool Plugin::guiSetSize(uint32_t width, uint32_t height) noexcept
+{
+    if (mApp == nullptr)
+        return false;
+
+    mApp->setWindowDimensions(static_cast<int>(width), static_cast<int>(height));
+    return true;
+}
+
+bool Plugin::guiGetSize(uint32_t *width, uint32_t *height) noexcept
+{
+    if (mApp == nullptr)
+        return false;
+
+    *width  = static_cast<uint32_t>(mApp->logicalWidth());
+    *height = static_cast<uint32_t>(mApp->logicalHeight());
+    return true;
+}
--- src/Plugin.h
+++ src/Plugin.h
@@ -5,6 +5,8 @@
 #include <clap/helpers/plugin.hh>
 #include <choc_ReenableAllWarnings.h>
 
+#include <visage/app.h>
+
 using ClapPlugin =
     clap::helpers::Plugin<clap::helpers::MisbehaviourHandler::Terminate, clap::helpers::CheckingLevel::Maximal>;
 
@@ -14,4 +16,21 @@ class Plugin : public ClapPlugin
     static clap_plugin_descriptor descriptor;
 
     explicit Plugin(const clap_host *host);
+    ~Plugin() override;
+
+protected:
+    bool implementsGui() const noexcept override { return true; }
+    bool guiIsApiSupported(const char* api, bool is_floating) noexcept override;
+    bool guiCreate(const char* api, bool is_floating) noexcept override;
+    void guiDestroy() noexcept override;
+    bool guiSetParent(const clap_window* window) noexcept override;
+    bool guiSetScale(double /*scale*/) noexcept override { return false; }
+    bool guiCanResize() const noexcept override { return true; }
+    bool guiGetResizeHints(clap_gui_resize_hints_t* hints) noexcept override;
+    bool guiAdjustSize(uint32_t* width, uint32_t* height) noexcept override;
+    bool guiSetSize(uint32_t width, uint32_t height) noexcept override;
+    bool guiGetSize(uint32_t* width, uint32_t* height) noexcept override;
+
+private:
+    std::unique_ptr<visage::ApplicationWindow> mApp;
 };
