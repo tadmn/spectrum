5d8aacf1209ad8aea003923a4aec8670fdfc3913
Tad Nicol <34101867+tadmn@users.noreply.github.com>
2025-03-08 15:23:59 -0800

    Added simple ballistics

--- src/MainGuiFrame.h
+++ src/MainGuiFrame.h
@@ -35,6 +35,9 @@ public:
         constexpr auto kMinFreq = 10.0;
         constexpr auto kMaxFreq = 25'000.0;
 
+        auto const kAttackRate = std::clamp(3.0 * canvas.deltaTime(), 0.0, 1.0);
+        auto const kReleaseRate = std::clamp(0.5 * canvas.deltaTime(), 0.0, 1.0);
+
         auto const logMinFreq = std::log10(kMinFreq);
         auto const logMaxFreq = std::log10(kMaxFreq);
 
@@ -43,6 +46,7 @@ public:
         for (int i = 0; i < (int)kNumFftBins; ++i)
         {
             auto const freq = i * deltaFreq;
+
             if (freq < kMinFreq)
             {
                 mLine.setXAt(i, 0.f);
@@ -58,9 +62,24 @@ public:
                 auto const x = (std::log10(freq) - logMinFreq) / (logMaxFreq - logMinFreq);
                 mLine.setXAt((int)i, static_cast<float>(x * mLine.width()));
 
-                auto const mag = 20.0 * std::log10(std::abs(fftOut[(uint)i]));
-                auto const y = 1.0 - mag / 35.0;
-                mLine.setYAt((int)i, static_cast<float>(y * mLine.height()));
+                auto mag = std::abs(fftOut[(uint)i]);
+                auto y = 0.0;
+                if (mag > 0.0)
+                {
+                    auto const dB = 20.0 * std::log10(mag);
+                    y = dB / 35.0; // [0,1+]
+                    auto const oldY = (mLine.height() - mLine.yAt(i)) / mLine.height(); // [0,1]
+
+                    if (y > oldY)
+                    {
+                        y = kAttackRate * y + (1.0 - kAttackRate) * oldY;
+                    } else
+                    {
+                        y = kReleaseRate * y + (1.0 - kReleaseRate) * oldY;
+                    }
+                }
+
+                mLine.setYAt((int)i, static_cast<float>((1.0 - y) * mLine.height()));
             }
         }
 
