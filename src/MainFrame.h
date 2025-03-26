f14769f7f65ae3194870f9979fe9884753338781
Tad Nicol <34101867+tadmn@users.noreply.github.com>
2025-03-26 14:40:45 -0700

    Add gradient alpha overlay

--- src/MainFrame.h
+++ src/MainFrame.h
@@ -7,6 +7,17 @@
 
 #include <visage/widgets.h>
 
+struct GradientOverlay : visage::Frame {
+    void draw(visage::Canvas& c) override {
+        const auto brush = visage::Brush::linear(visage::Gradient(0x00000000, 0xff000000),
+                                                 { static_cast<float>(width()) / 2, 0 },
+                                                 { static_cast<float>(width() / 2),
+                                                   static_cast<float>(height()) });
+        c.setColor(brush);
+        c.fill(0, 0, width(), height());
+    }
+};
+
 class MainFrame : public visage::Frame {
   public:
     MainFrame(AnalyzerProcessor& p) :
@@ -17,6 +28,7 @@ class MainFrame : public visage::Frame {
         setPalette(&mPalette);
 
         addChild(mAnalyzer);
+        addChild(mGradientOverlay);
 
         mButton.onToggle() = [this](visage::Button*, bool on) {
             mSettings.setVisible(on);
@@ -33,12 +45,16 @@ class MainFrame : public visage::Frame {
         mButton.setBounds(15, 15, 65, 65);
         mSettings.setBounds(mButton.right() + 15, 0, width() - mButton.width() - 15, 100);
         mAnalyzer.setBounds(0, 0, width(), height());
+
+        const auto h = height() / 3.5;
+        mGradientOverlay.setBounds(0, height() - h, width(), h);
     }
 
   private:
     visage::Palette mPalette;
 
     AnalyzerFrame mAnalyzer;
+    GradientOverlay mGradientOverlay;
     visage::ToggleIconButton mButton;
 
     SettingsFrame mSettings;
