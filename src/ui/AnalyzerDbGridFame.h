27cac635d90cee23a565ba13d50a9b80dbafb7b5
Tad Nicol <34101867+tadmn@users.noreply.github.com>
2025-04-04 15:52:54 -0700

    Improve dB grid

--- src/ui/AnalyzerDbGridFame.h
+++ src/ui/AnalyzerDbGridFame.h
@@ -18,10 +18,15 @@ public:
   void draw(visage::Canvas& canvas) override {
     canvas.setNativePixelScale();
 
-    // Find the first multiple of 10 above mMinDb
-    const auto firstTickDb = static_cast<int>(std::ceil(mMinDb / 10.f)) * 10;
+    {
+      // Set the horizontal fade in/out to transparent for aesthetics
+      const auto c0 = visage::Color(0xffffff).withAlpha(0);
+      const auto c1 = c0.withAlpha(0.2);
+      canvas.setBrush(visage::Brush::horizontal(visage::Gradient(c0, c1, c1, c1, c1, c0)));
+    }
 
     // Draw lines for each 10 dB step
+    const auto firstTickDb = static_cast<int>(std::ceil(mMinDb / 10.f)) * 10;
     for (int dbValue = firstTickDb; dbValue <= mMaxDb; dbValue += 10) {
       // Calculate vertical position (convert from dB to y coordinate)
       float y0to1 = tb::to0to1(static_cast<float>(dbValue), mMinDb, mMaxDb);
@@ -30,13 +35,6 @@ public:
       const auto y = std::round(lineY * canvas.dpiScale());
 
       // Draw the horizontal line
-      const visage::Color color(0xffffffff);
-
-      const float w = nativeWidth();
-      const float h = nativeHeight();
-
-      canvas.setBrush(visage::Brush::linear(color.withAlpha(0), color.withAlpha(0.2),
-                                            { 0, h / 2.f }, { 0.2f * w, h / 2.f }));
       canvas.segment(0, y, nativeWidth(), y, 1, false);
     }
   }
